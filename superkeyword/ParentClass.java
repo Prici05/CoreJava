5401d9c63566dc4fbe93f45e252f44f97e26c814
Prici05 <161338593+Prici05@users.noreply.github.com>
2024-10-03 18:04:25 +0530

    Create ParentClass.java

---
+++ superkeyword/ParentClass.java
@@ -0,0 +1,17 @@
+package Superkeyword;
+
+public class ParentClass {
+
+    public ParentClass()
+    {
+        System.out.println("I am the parent class constructor");
+    }
+
+    String name = "Prici";
+
+    public void getData()
+    {
+        System.out.println("I am the parent class");
+    }
+
+}
