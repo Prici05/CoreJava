abe39ab1eab4cc919794c7c518475dfa20f32397
Prici05 <161338593+Prici05@users.noreply.github.com>
2024-10-03 18:02:05 +0530

    Create AbstractClass.java

---
+++ Abstract/AbstractClass.java
@@ -0,0 +1,19 @@
+package Abstract;
+public abstract class AbstractClass {
+
+    public void engine()
+    {
+        System.out.println("Engine rules");
+    }
+
+    public void Safety()
+    {
+        System.out.println("Safety rules");
+
+    }
+
+    public abstract void color();
+
+
+
+}
